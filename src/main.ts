19d9fe844fd7371d219c61debb564d727bb1bb07
johnlindquist <johnlindquist@gmail.com>
2015-12-06 20:18:50 -0700

    locking versions with shrinkwrap

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+//These first 3 lines will be deprecated by the final release
 import 'reflect-metadata'
 import 'zone.js'
+import "angular2/angular2";
 
 import {Component} from "angular2/core";
 import {bootstrap} from "angular2/platform/browser";
